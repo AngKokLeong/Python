1e30344b018ded75057db460766c59d525df7af0
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2024-09-08 12:56:24 +0000

    added python implementation for generating 5 by 5 dataframe with random numbers from 100 to 200

---
+++ lab-4/create_pandas_dataframe/create-dataframe-with-numpy-array-and-default-index.py
@@ -0,0 +1,24 @@
+import pandas
+import numpy
+
+pandas_dataframe_array = []
+
+
+number_of_columns: int = 5
+number_of_rows: int = 5
+
+matrix: list[list] = []
+
+for row_index in range(number_of_rows):
+    # define the row and then column
+    new_row: list = []
+    for column_index in range(number_of_columns):
+        random_number: int = numpy.random.default_rng().integers(100, 200,size=1, endpoint=True)
+        new_row.append(random_number)
+    
+    matrix.append(new_row)
+
+
+pandas_dataframe = pandas.DataFrame(matrix)
+
+print(pandas_dataframe)
