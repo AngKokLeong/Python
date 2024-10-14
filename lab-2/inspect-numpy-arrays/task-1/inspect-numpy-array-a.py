28043c35faa330bc4a89fbd38743c54343aea32c
Ang Kok Leong <ang.kok.leong.development@gmail.com>
2024-10-14 11:13:40 +0000

    added numpy.array() to create numpy array and other properties and function to inspect the numpy array

--- lab-2/inspect-numpy-arrays/task-1/inspect-numpy-array-a.py
+++ lab-2/inspect-numpy-arrays/task-1/inspect-numpy-array-a.py
@@ -1,14 +1,14 @@
 import numpy
 
 
-float_ndarray: numpy.ndarray = [1.55, 1.73, 1.9, 1.72, 1.66, 1.8]
-tuple_ndarray: numpy.ndarray = [
+float_ndarray: numpy.ndarray = numpy.array([1.55, 1.73, 1.9, 1.72, 1.66, 1.8])
+tuple_ndarray: numpy.ndarray = numpy.array([
     ('red', 'green', 'yellow', 'blue'),
     (True, False, False, True),
     ('2017-10-01', '2017-11-11', '2917-12-23', '2018-03-31')
-]
+])
 
-integer_tuple_ndarray: numpy.ndarray = [
+integer_tuple_ndarray: numpy.ndarray = numpy.array([
     [
         (10, 9, 8, 7),
         (6, 5, 4, 3)
@@ -17,17 +17,39 @@ integer_tuple_ndarray: numpy.ndarray = [
         (1,2,3,4),
         (5,6,7,8)
     ]
-]
+])
+
+data_1_ndarray: numpy.ndarray = numpy.loadtxt("../../Lab2_Dataset/data1.txt", delimiter=",", dtype=[("asd",'<U12'), ("ds",'<U12'), ("qwe",'<U12'),("zxc",'<U12')])
 
 
-data_1_ndarray: numpy.ndarray = numpy.loadtxt("../../Lab2_Dataset/data1.txt")
 
+print("Size")
+print(float_ndarray.size)
+print(tuple_ndarray.size)
+print(integer_tuple_ndarray.size)
+print(data_1_ndarray.size)
 
+print("")
 
-print(float_ndarray)
+print("shape")
+print(float_ndarray.shape)
+print(tuple_ndarray.shape)
+print(integer_tuple_ndarray.shape)
+print(data_1_ndarray.shape)
 
-print(tuple_ndarray)
+print("")
 
-print(integer_tuple_ndarray)
+print("len")
+print(len(float_ndarray))
+print(len(tuple_ndarray))
+print(len(integer_tuple_ndarray))
+print(len(data_1_ndarray))
 
-print(data_1_ndarray)
+print("")
+
+print("ndim")
+print(float_ndarray.ndim)
+print(tuple_ndarray.ndim)
+print(integer_tuple_ndarray.ndim)
+print(data_1_ndarray.ndim)
+
